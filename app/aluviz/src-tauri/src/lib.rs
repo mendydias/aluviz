4b78e731058414d76accb94078a1f9df896672c9
Umendya Ranmal Dias <dev.mendy.das@gmail.com>
2024-12-14 13:34:50 +0530

    feat: basic memory data types done

--- app/aluviz/src-tauri/src/lib.rs
+++ app/aluviz/src-tauri/src/lib.rs
@@ -12,3 +12,66 @@ pub fn run() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+pub mod memory {
+
+    pub struct BasicMemory {
+        cell: usize,
+        columns: usize,
+    }
+
+    pub struct MemCapacity {
+        cell: usize,
+        columns: usize,
+    }
+
+    pub enum Customizer {
+        MemCustomizer,
+    }
+
+    pub trait MemCustomizer {
+        fn default_mem_capacity() -> MemCapacity;
+    }
+
+    impl BasicMemory {
+        pub fn new(mem_cap: MemCapacity) -> Self {
+            let cell = mem_cap.cell;
+            let columns = mem_cap.columns;
+            BasicMemory { cell, columns }
+        }
+
+        pub fn cap(&self) -> usize {
+            self.cell * self.columns
+        }
+    }
+
+    impl MemCustomizer for Customizer {
+        fn default_mem_capacity() -> MemCapacity {
+            MemCapacity {
+                cell: 8,
+                columns: 64,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::memory::{BasicMemory, Customizer};
+
+    #[test]
+    fn test_memory_init() {
+        let mem_customizer = Customizer::MemCustomizer();
+        let mem = BasicMemory::new(Customizer::default_mem_capacity());
+        let default_cap: usize = 64 * 8;
+        assert_eq!(mem.cap(), default_cap);
+    }
+
+    #[test]
+    fn test_memory_bin_setup() {
+        let mem = memory::BasicMemory::new(memory::default_mem_capacity());
+        let bin_count = 4;
+        let bin_width = 128;
+        mem.allocate_bins()
+    }
+}
